a353c2d793fdc09318c09ec32a7197b3a353a034
VASIA <Vasia.danilyuk@gmail.com>
2019-05-10 23:12:09 +0300

    /5

--- CourseWork2/Calculation.cpp
+++ CourseWork2/Calculation.cpp
@@ -15,6 +15,12 @@ double Calculation::getExactResolve(double x, double t)
 	return result;
 }
 
+double Calculation::getApproximateSolution(int i, int k, double h, double tau, double w_i, double w_i_p, double w_i_m)
+{
+	double result = ((w_i_m - 2 * w_i + w_i_p) * tau / (h * h)) + (w_i*tau*(w_i_p - w_i_m) / (2 * h)) + w_i;
+	return result;
+};
+
 Calculation::~Calculation()
 {
 }
