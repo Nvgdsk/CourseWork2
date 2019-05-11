5adaa9b7627a2fa3fd27921fbe8c9ef77ff3d84c
VASIA <Vasia.danilyuk@gmail.com>
2019-05-11 14:08:21 +0300

    partical

--- CourseWork2/Calculation.cpp
+++ CourseWork2/Calculation.cpp
@@ -7,15 +7,17 @@ Calculation::Calculation()
 {
 	Calculation::a = 4;
 	Calculation::b = 5;
+	Calculation::tau = 0.00005;
+	Calculation::h = 0.01;
 }
 
 double Calculation::getExactResolve(double x, double t)
 {
-	double result = (Calculation::a - x) / (Calculation::b + x);
+	double result = (Calculation::a - x) / (Calculation::b + t);
 	return result;
 }
 
-double Calculation::getApproximateSolution(int i, int k, double h, double tau, double w_i, double w_i_p, double w_i_m)
+double Calculation::getApproximateSolution(int i, int k, double w_i, double w_i_p, double w_i_m)
 {
 	double result = ((w_i_m - 2 * w_i + w_i_p) * tau / (h * h)) + (w_i*tau*(w_i_p - w_i_m) / (2 * h)) + w_i;
 	return result;
--- CourseWork2/Calculation.h
+++ CourseWork2/Calculation.h
@@ -4,11 +4,13 @@ class Calculation
 public:
 	int a;
 	int b;
+	double tau;
+	double h;
 
 	Calculation();
 
-	double getExactResolve(double x, double t) {};
-	double getApproximateSolution(int i, int k, double h, double tau, double w_i, double w_i_p, double w_i_m) {};
+	double getExactResolve(double x, double t);
+	double getApproximateSolution(int i, int k, double w_i, double w_i_p, double w_i_m);
 
 	~Calculation();
 };
